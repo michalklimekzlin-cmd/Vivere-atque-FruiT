a397ea0d77eaa582b8f16be96351927a2055a74a
michalklimekzlin-cmd <michalklimek.zlin@gmail.com>
2025-11-03 17:22:07 +0100

    Delete feeds.pedrovci.js

--- feeds.pedrovci.js
+++
@@ -1,7 +0,0 @@
-import { viri } from "./viri.mind.js";
-export const Pedrovci = {
-  emotion(emotion, value=0.5, note="") {
-    viri.learn("pedrovci", { emotion, value, note });
-  }
-};
-globalThis.VAFI ||= {}; globalThis.VAFI.pedrovci = Pedrovci;
