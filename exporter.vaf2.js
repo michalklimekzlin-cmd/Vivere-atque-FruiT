85a622549975495166a3092ca96c9013bbb396ad
michalklimekzlin-cmd <michalklimek.zlin@gmail.com>
2025-11-03 17:21:23 +0100

    Delete exporter.vaf2.js

--- exporter.vaf2.js
+++
@@ -1,9 +0,0 @@
-import { getBlueprint } from "./world.builder.js";
-export function exportJSON(){
-  const blob = new Blob([JSON.stringify(getBlueprint(),null,2)], {type:"application/json"});
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement("a");
-  a.href = url; a.download = "viri-blueprint.json"; a.click();
-  URL.revokeObjectURL(url);
-}
-globalThis.VAFI ||= {}; globalThis.VAFI.exporter = { exportJSON };
