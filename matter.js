ee4562808f2ad7f7cdd2a4d1c19258ed70880dc4
michalklimekzlin-cmd <michalklimek.zlin@gmail.com>
2025-11-03 17:25:08 +0100

    Delete matter.js

--- matter.js
+++
@@ -1,9 +0,0 @@
-export class Matter{
-  constructor(ecs, bus){
-    this.ecs=ecs; this.bus=bus; this.iskra=0; this.quantaPerBrick=6; this.lastPos={x:0,y:0};
-    bus.on('impulse',({pos})=>{ this.iskra+=1; this.lastPos=pos; this._updateHud(); if(this.iskra>=this.quantaPerBrick){ this.iskra-=this.quantaPerBrick; this.spawnBrick(pos); this._updateHud(); } });
-  }
-  _updateHud(){ const el=document.getElementById('iskra'); if(el) el.textContent=String(this.iskra); }
-  spawnBrick(pos){ const id=this.ecs.create(); this.ecs.add(id,{ block:true, x:pos.x, y:pos.y, size:10 }); }
-  render(glview){ /* replaced by glyph rectangles in app.js */ }
-}
