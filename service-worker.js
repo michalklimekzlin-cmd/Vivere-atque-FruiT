8ca9e81ad97f4398f619bb3c93dc95a734e02f53
michalklimekzlin-cmd <michalklimek.zlin@gmail.com>
2025-11-09 20:43:43 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,23 @@
-// Vivere atque FruiT offline worker
 const CACHE_NAME = 'vaft-cache-v1';
 const urlsToCache = [
   './',
   './index.html',
   './style.css',
+  './hlavoun.js',
   './manifest.json',
   './VafiT-gallery/index.html',
   './VafiT-gallery/style.css',
   './VafiT-gallery/app.js'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
