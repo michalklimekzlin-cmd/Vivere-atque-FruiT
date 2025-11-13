3a5a5c4e24f685f44696996a9d9146c7ad79a09e
michalklimekzlin-cmd <michalklimek.zlin@gmail.com>
2025-11-13 17:11:31 +0100

    Create worlds-select.js

---
+++ apps/worlds-select.js
@@ -0,0 +1,6 @@
+VAFT_APP('worlds-select', {
+  title: 'Výběr světů',
+  open() {
+    alert('Výběr světů se řeší přímo v gridu (#11). Tady může být později detailní seznam a statistiky světů.');
+  }
+});
