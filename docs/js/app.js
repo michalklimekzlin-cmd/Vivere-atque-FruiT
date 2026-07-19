a9e69b3b90c92abf2329b1131c71629741a5372f
Vivere atque Frui¡’T <michalklimek.zlin@gmail.com>
2026-07-19 17:20:31 +0200

    Update app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -3,3 +3,4 @@
 // CHT 360°‰ používá hlavní kreslení v ./aplikace.js.
 // Tento modul zůstává jako bezpečný most pro starý odkaz v index.html.
 window.dispatchEvent(new CustomEvent("cht360.bridge.ready"));
+
