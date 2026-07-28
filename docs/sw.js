7b57f0246c380877d1cf5782152cf44ececd855d
Vivere atque Frui¡’T <michalklimek.zlin@gmail.com>
2026-07-28 06:17:29 +0200

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,40 +1,9 @@
-const CACHE_NAME = "glyph-cht-360-guard-v2";
+"use strict";
 
-const CORE_FILES = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.webmanifest",
-  "./icon.svg"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(CORE_FILES))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys()
-      .then((keys) =>
-        Promise.all(
-          keys
-            .filter((key) => key !== CACHE_NAME)
-            .map((key) => caches.delete(key))
-        )
-      )
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
-});
+/*
+  Kompatibilní vstup pro starší část CHT, která ještě registruje ./sw.js.
+  Skutečná společná offline logika je pouze v service-worker.js.
+  Díky tomu stará registrace nemaže cache ostatních PWA a nehledá
+  neexistující styles.css, app.js ani manifest.webmanifest.
+*/
+importScripts("./service-worker.js");
